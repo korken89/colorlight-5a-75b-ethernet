0855a21a7332edcf2a44edfa2cbb3f6a4619ae5f
Emil Fresk <emil.fresk@gmail.com>
2025-01-20 22:06:03 +0100

    Remove old stuff

--- gateware/cli.py
+++ gateware/cli.py
@@ -3,12 +3,10 @@ import os
 import sys
 
 from .platform.colorlight_5a_75b_v82 import Colorlight_5A75B_R82Platform
-from .test.blinky import *
 from .pll_timer import PllTimer
 
 def main():
     Colorlight_5A75B_R82Platform().build(PllTimer(), do_program=True, verbose=True)
-
 
 if __name__ == "__main__":
     main()
