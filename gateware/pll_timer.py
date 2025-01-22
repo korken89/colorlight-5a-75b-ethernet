bb898e2560cbd7bf3ffeb25efacf0d037f49fbbe
Emil Fresk <emil.fresk@gmail.com>
2025-01-22 19:10:26 +0100

    Testing delayg

--- gateware/pll_timer.py
+++ gateware/pll_timer.py
@@ -1,4 +1,4 @@
-from amaranth import Elaboratable, Signal, Module, ClockSignal
+from amaranth import Elaboratable, Signal, Module, ClockSignal, Instance
 from amaranth.build import Platform
 from amaranth.lib.fifo import AsyncFIFOBuffered
 from .ecp5pll import ECP5PLL, ECP5PLLConfig
@@ -9,11 +9,13 @@ class PllTimer(Elaboratable):
         led1 = platform.request("led", 0)#, xdr=2)
         # led4 = platform.request("led", 3)
 
-        w = 18
+        w = 8
         
 
         timer1 = Signal(25)
         fifo_buf = Signal(w)
+
+        led_sig = Signal()
 
         m = Module()
 
@@ -23,12 +25,21 @@ class PllTimer(Elaboratable):
                                        ECP5PLLConfig("sync", 25),
                                    ])
 
+        # Delay
+        m.submodules.delay = Instance("DELAYG",
+                    p_DEL_MODE  = "SCLK_ALIGNED",
+                    p_DEL_VALUE = 0,
+                    i_A         = led_sig,
+                    o_Z         = led1.o,
+                )
+
         # This should use 2 EBRs
         m.submodules.fifo = fifo = AsyncFIFOBuffered(
             width=w, depth=2049, r_domain="fast", w_domain="sync", exact_depth=True)
 
         # Write the FIFO using the timer data
         m.d.sync += timer1.eq(timer1 + 1)
+
         with m.If(fifo.w_rdy):
             m.d.comb += fifo.w_data.eq(timer1[25-w:25])
         m.d.comb += fifo.w_en.eq(1)
@@ -40,6 +51,6 @@ class PllTimer(Elaboratable):
         m.d.comb += fifo.r_en.eq(1)
 
         # Combinatorial logic
-        m.d.comb += led1.o.eq(fifo_buf[-1])
+        m.d.comb += led_sig.eq(fifo_buf[-1])
 
         return m
